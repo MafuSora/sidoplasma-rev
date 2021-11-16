f9880a35b00ad880c7181f6470be667a2342e468
MafuSora <hafizh.maulana.ub@gmail.com>
2021-11-16 18:22:59 +0700

    aaa

--- src/components/About_Us/about.js
+++ src/components/About_Us/about.js
@@ -13,8 +13,70 @@ const Faskes_Konten_1=()=>{
     { name: 'Radio', value: '4' },
     ];
     return(
-        <div className="container-xxl card cardcolor" >
+        <div className="framebody">
+            <div>
+                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators p-3">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img src="https://th.bing.com/th/id/OIP.VGxLRyJbW-GrRGN-lPUIvgHaE8?pid=ImgDet&rs=1" class="carousel slide m-4 rounded ukuran" alt="..."/>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                        </div>
+                        <div class="carousel-item">
+                        <img src="https://th.bing.com/th/id/OIP.VGxLRyJbW-GrRGN-lPUIvgHaE8?pid=ImgDet&rs=1" class="carousel slide m-4 rounded ukuran" alt="..."/>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
+                        </div>
+                        <div class="carousel-item">
+                        <img src="https://th.bing.com/th/id/OIP.VGxLRyJbW-GrRGN-lPUIvgHaE8?pid=ImgDet&rs=1" class="carousel slide m-4 rounded ukuran" alt="..."/>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
                 
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <h1 class="display-1 text-center ahaha">Alur Pemesanan </h1>
+                    <div class="carousel-inner">
+                            <div class="carousel-item active">
+                            <img src="https://th.bing.com/th/id/OIP.VGxLRyJbW-GrRGN-lPUIvgHaE8?pid=ImgDet&rs=1" class="carousel slide m-4 rounded ukuran2" alt="..."/>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://th.bing.com/th/id/OIP.VGxLRyJbW-GrRGN-lPUIvgHaE8?pid=ImgDet&rs=1" class="carousel slide m-4 rounded ukuran2" alt="..."/>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://th.bing.com/th/id/OIP.VGxLRyJbW-GrRGN-lPUIvgHaE8?pid=ImgDet&rs=1" class="carousel slide m-4 rounded ukuran2" alt="..."/>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                    </div>
+                </div>
+            </div>
         </div>
     
     );
