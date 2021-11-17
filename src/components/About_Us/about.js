7c6aa7363afa4baa5506b76c4919208da9f31b79
MafuSora <hafizh.maulana.ub@gmail.com>
2021-11-17 18:29:50 +0700

    Update about Us

--- src/components/About_Us/about.js
+++ src/components/About_Us/about.js
@@ -2,39 +2,69 @@ import { Button, ButtonGroup,ToggleButton } from 'react-bootstrap';
 import reactDom from 'react-dom';
 import react,{useState} from 'react';
 import suntik from '../img/suntik.png';
+import './about.css'
+import { Card, Row, Col,CardGroup } from 'react-bootstrap';
 const Faskes_Konten_1=()=>{
     const [checked, setChecked] = useState(false);
     const [radioValue, setRadioValue] = useState('1');
+
 
     
     return(
         <div className="framebody">
             <div>
-                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-indicators p-3">
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    </div>
-                    <div class="carousel-inner">
-                        <div className="background">
-                        {/* <img src="https://i.pinimg.com/564x/22/2e/40/222e4048c228885608cc11aa7cc608b2.jpg" class="carousel slide m-4 rounded ukuran" alt="..." className="suntik"/> */}
+                        <div className="background p-2">
+                        {/* <img src="https://i.pinimg.com/564x/22/2e/40/222e4048c228885608cc11aa7cc608b2.jpg" className="carousel slide m-4 rounded ukuran" alt="..." className="suntik"/> */}
                             <h1>Selamat Datang</h1>
                             <p>Penggunaan media berupa website yang terintegrasi dengan auto message digunakan untuk pendataan calon pendonor plasma konvalesen COVID-19 oleh berbagai fasilitas kesehatan dan dapat diakses oleh Palang Merah Indonesia (PMI).</p>
-                            <a href="#" class="btn btn-lg btn-danger"> Gabung Sekarang </a>
+                            <a href="#" className="btn btn-lg btn-danger"> Gabung Sekarang </a>
                         </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
+                        <CardGroup className="p-2">
+                                <Card >
+                                    <Card.Img variant="top" src="holder.js/100px160" />
+                                    <Card.Body>
+                                    <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                        This is a wider card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer.
+                                    </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                    </Card.Footer>
+                                </Card>
+                                <Card>
+                                    <Card.Img variant="top" src="holder.js/100px160" />
+                                    <Card.Body>
+                                    <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                        This card has supporting text below as a natural lead-in to additional
+                                        content.{' '}
+                                    </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                    </Card.Footer>
+                                </Card>
+                                <Card>
+                                    <Card.Img variant="top" src="holder.js/100px160" />
+                                    <Card.Body>
+                                    <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                        This is a wider card with supporting text below as a natural lead-in to
+                                        additional content. This card has even longer content than the first to
+                                        show that equal height action.
+                                    </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                    </Card.Footer>
+                                </Card>
+                            </CardGroup>
             </div>
         </div>
+                            
+   
 
     );
 }
