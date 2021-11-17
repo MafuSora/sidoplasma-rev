32c6bf3f090968f082bc98ad832b1b3b8e9a385d
sidoplasma <sidoplasmateam@gmail.com>
2021-11-17 17:24:43 +0700

    gabisa background

--- src/components/About_Us/about real.js
+++ src/components/About_Us/about real.js
@@ -1,3 +1,4 @@
+
 import { Button, ButtonGroup,ToggleButton } from 'react-bootstrap';
 import reactDom from 'react-dom';
 import react,{useState} from 'react';
@@ -17,11 +18,14 @@ const Faskes_Konten_1=()=>{
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                     </div>
                     <div class="carousel-inner">
-                        <div className="background">
-                        {/* <img src="https://i.pinimg.com/564x/22/2e/40/222e4048c228885608cc11aa7cc608b2.jpg" class="carousel slide m-4 rounded ukuran" alt="..." className="suntik"/> */}
+                        <div class="carousel-item active">
+                        <img src={suntik} alt="sidoplasma" class="carousel slide m-4 rounded ukuran" class = "suntik" alt="..."  ></img>
+                        {/* <img src="https://th.bing.com/th/id/OIP.VGxLRyJbW-GrRGN-lPUIvgHaE8?pid=ImgDet&rs=1" class="carousel slide m-4 rounded ukuran" alt="..."/> */}
+                        <div class="container">
                             <h1>Selamat Datang</h1>
                             <p>Penggunaan media berupa website yang terintegrasi dengan auto message digunakan untuk pendataan calon pendonor plasma konvalesen COVID-19 oleh berbagai fasilitas kesehatan dan dapat diakses oleh Palang Merah Indonesia (PMI).</p>
                             <a href="#" class="btn btn-lg btn-danger"> Gabung Sekarang </a>
+                        </div>
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
@@ -39,3 +43,35 @@ const Faskes_Konten_1=()=>{
     );
 }
 export default Faskes_Konten_1
+
+import { Button, ButtonGroup,ToggleButton } from 'react-bootstrap';
+import reactDom from 'react-dom';
+import react,{useState} from 'react';
+import suntik from '../img/suntik.png';
+const Faskes_Konten_1=()=>{
+    const [checked, setChecked] = useState(false);
+    const [radioValue, setRadioValue] = useState('1');
+
+    
+    return(
+        <div className="framebody">
+            <div class ="carousel-inner">
+                <div class="carousel-item active"
+                style="background-image:url(../img/suntik.png);">
+                    <div class="container">
+                        <h1>Selamat Datang</h1>
+                        <p>
+                            Sidoplasma merupakan media berupa website yang terintegrasi dengan auto message digunakan untuk pendataan calon pendonor plasma konvalesen COVID-19 oleh berbagai fasilitas kesehatan dan dapat diakses oleh Palang Merah Indonesia (PMI)
+                        </p>
+                        <a href="#" class="btn btn-lg
+                        btn-danger"> Gabung Sekarang</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+export default Faskes_Konten_1
+
+
